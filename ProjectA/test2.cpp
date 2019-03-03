ddc7de356c06255cded65931297687394d8a67e1
TianxingW <pmxtw5@nottingham.ac.uk>
2019-03-03 17:26:25 +0000

    tx commit

---
+++ ProjectA/test2.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lx on 2019/3/3.
+//
+
+#include "test2.hpp"
---
+++ ProjectA/test2.hpp
@@ -0,0 +1,14 @@
+//
+// Created by lx on 2019/3/3.
+//
+
+#ifndef PROJECTA_TEST2_HPP
+#define PROJECTA_TEST2_HPP
+
+
+class test2 {
+
+};
+
+
+#endif //PROJECTA_TEST2_HPP
