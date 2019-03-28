0d09811e1a3aab4fec263f1416981d455f80f132
ChrisBroughall <pmxcb5@nottingham.ac.uk>
2019-03-28 16:49:18 +0000

    Euler updated

--- MC/main.cpp
+++ MC/main.cpp
@@ -2,18 +2,31 @@
 #include <memory>
 #include "OptionInfo.hpp"
 #include "RNG.hpp"
-#include "BlackScholes.hpp"
+#include "MonteCarlo.hpp"
 #include "EulerMethod.hpp"
+#include "BlackScholes.hpp"
 #include <iomanip>
 
+void initOptionInfo();
+void runMonteCarloSimulation();
+void runRNG();
+void runEuler();
+void runBlackScholes();
 
+int main ()
+{
+    initOptionInfo();
+    runMonteCarloSimulation();
+    runEuler();
+    runBlackScholes();
+    return 0;
+}
 
 void initOptionInfo()
 {
     shared_ptr<OptionInfo> optionInfo=make_shared<OptionInfo>();
-    cout<<  optionInfo << endl;
+    cout<<  optionInfo<<endl;
 }
-
 
 void runEuler()
 {
@@ -41,23 +54,52 @@ void runEuler()
 
 }
 
+void runMonteCarloSimulation()
+{
+    unique_ptr<MonteCarlo> mc;
+    shared_ptr<OptionInfo> optionInfo=make_shared<OptionInfo>();
+    int size=5000;
+    double dt=1.0/365.0;
+    double t=0.0;
+    vector<double> Sj(size,0.0),Vj(size,0.0),SjMinusDT(size,0.0),SjPlusDT(size,0.0);
+    vector<double> VjMinusDT(size,0.0),VjPlusDT(size,0.0);
+    Sj= mc->genStockPrices(optionInfo->getS0(), optionInfo->getT(), optionInfo->getR(),optionInfo->getSigma(),size);
+    for (int j = 0; j < Sj.size(); ++j)
+    {
+    //        cout<<Sj[j]<<endl;
+            Vj[j]=optionInfo->payOff(Sj[j]);
+
+    }
+
+    double avgST = accumulate( Sj.begin(), Sj.end(), 0.0)/Sj.size();
+    cout << "Approximate Stock Price at T : " << avgST << endl;
+
+    double avgVT = accumulate( Vj.begin(), Vj.end(), 0.0)/Vj.size();
+    cout << "MC Call Price: " << exp(-optionInfo->getR()*(optionInfo->getT()-t))*avgVT << endl;
+
+    //DELTA SIMULATION
+    SjMinusDT= mc->genStockPrices(optionInfo->getS0(), optionInfo->getT()-dt, optionInfo->getR(),optionInfo->getSigma(),size);
+    SjPlusDT = mc->genStockPrices(optionInfo->getS0(), optionInfo->getT()+dt, optionInfo->getR(),optionInfo->getSigma(),size);
+    for (int j = 0; j < Sj.size(); ++j)
+    {
+        //        cout<<Sj[j]<<endl;
+        VjMinusDT[j]=optionInfo->payOff(SjMinusDT[j]);
+        VjPlusDT[j]=optionInfo->payOff(SjPlusDT[j]);
+    }
+
+    double avgVTMinusDT = accumulate( VjMinusDT.begin(), VjMinusDT.end(), 0.0)/VjMinusDT.size();
+    double avgVTPlusDT = accumulate( VjPlusDT.begin(), VjPlusDT.end(), 0.0)/VjPlusDT.size();
+    cout << "MC Delta: " << ((exp(-optionInfo->getR()*(optionInfo->getT()+dt-t))*avgVTPlusDT)-(exp(-optionInfo->getR()*(optionInfo->getT()-dt-t))*avgVTMinusDT))/(2*dt) << endl;
+
+}
 
 void runBlackScholes()
 {
     unique_ptr<BlackScholes> bs;
     shared_ptr<OptionInfo> optionInfo=make_shared<OptionInfo>();
     vector<double> values= bs->calcExactValues(optionInfo->getS0(), optionInfo->getK(), optionInfo->getR(), optionInfo->getSigma(),
-                                               optionInfo->getT());
+                                  optionInfo->getT());
     cout<<"BS Call Price: "<<values[0]<<endl;
-
-
-}
-
-int main (){
-
-    initOptionInfo();
-    runEuler();
-    runBlackScholes();
-    return 0;
+    cout<<"BS Delta: "<<values[1]<<endl;
 
 }
