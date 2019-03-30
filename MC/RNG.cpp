e090be4e7e791c551e361d3ab0650bcc90ebdc12
Jeet <pmxhs9@nottingham.ac.uk>
2019-03-30 16:50:46 +0000

    fixed indexing error.

--- MC/RNG.cpp
+++ MC/RNG.cpp
@@ -11,7 +11,7 @@ vector<double> RNG::rngUsingBM (int size)
     random_device rd;
     mt19937 gen(rd());
     uniform_real_distribution<> distribution(0,1);
-    for (int i = 0; i < size; i=i+2)
+    for (int i = 0; i < size-1; i=i+2)
     {
         double r=distribution(gen);
         double phi=distribution(gen);
